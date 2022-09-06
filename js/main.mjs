a57b05eb5c7470e7a8722f8731637fe9c6fb21ed
manalidalvi12 <manalidalvi12@gmail.com>
2022-09-06 17:41:59 +0530

    UPDATE: Address Form created

--- js/main.mjs
+++ js/main.mjs
@@ -2,10 +2,12 @@ import { addHeader } from "./components/Header.mjs";
 import { getPersonalDetails } from "./components/PersonalForm.mjs";
 import { getGovtForm } from "./components/GovtForm.mjs";
 import { getLoginInfo } from "./components/LoginForm.mjs";
+import { getAddressDetails } from "./components/AddressForm.mjs";
 
 window.onload = () => {
     addHeader();
     // getPersonalDetails();
     // getGovtForm();
-    //getLoginInfo();
+    // getLoginInfo();
+    // getAddressDetails();
 }
