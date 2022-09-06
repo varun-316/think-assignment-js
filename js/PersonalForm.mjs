282dd2da7cd83695ac2a4900cc8be1784642fece
manalidalvi12 <manalidalvi12@gmail.com>
2022-09-06 16:43:10 +0530

    UPDATE: GovtForm created

--- js/PersonalForm.mjs
+++ js/PersonalForm.mjs
@@ -1,43 +1,48 @@
 export function getPersonalDetails(){
 
-    var br = document.createElement("br");
-    var form = document.createElement("form");
-    form.setAttribute("method","post");
-    form.setAttribute("action","GovtForm.js");
+    const br = document.createElement("br");
 
-    var fname = document.createElement("input");
+    const steps = document.createElement("p");
+    steps.innerHTML = "Step 1 of 3";
+
+    const form = document.createElement("form");
+    form.setAttribute("method","post");
+    form.setAttribute("action","GovtForm.mjs");
+
+    const fname = document.createElement("input");
     fname.setAttribute("type","text");
     fname.setAttribute("name","fname");
     fname.setAttribute("placeholder","First Name");
 
-    var lname = document.createElement("input");
+    const lname = document.createElement("input");
     lname.setAttribute("type","text");
     lname.setAttribute("name","lname");
     lname.setAttribute("placeholder","Last Name");
 
-    var contact = document.createElement("input");
+    const contact = document.createElement("input");
     contact.setAttribute("type","text");
     contact.setAttribute("name","contact");
     contact.setAttribute("maxlength","10");
     contact.setAttribute("placeholder","Contact Details");
 
-    var email = document.createElement("input");
+    const email = document.createElement("input");
     email.setAttribute("type","email");
     email.setAttribute("name","emailId");
     email.setAttribute("placeholder","Email");
 
-    var submit = document.createElement("input");
+    const submit = document.createElement("input");
     submit.setAttribute("type","submit");
-    submit.setAttribute("value","submit");
+    submit.setAttribute("value","Next");
 
+    form.appendChild(steps);
     form.appendChild(fname);
-    form.appendChild(br);
+    form.appendChild(br.cloneNode());
     form.appendChild(lname);
-    form.appendChild(br);
+    form.appendChild(br.cloneNode());
     form.appendChild(contact);
-    form.appendChild(br);
+    form.appendChild(br.cloneNode());
     form.appendChild(email);
-    form.appendChild(br);
+    form.appendChild(br.cloneNode());
     form.appendChild(submit);    
 
     document.getElementById("root").appendChild(form);
--- js/main.mjs
+++ js/main.mjs
@@ -1,5 +1,9 @@
 import { addHeader } from "./components.mjs";
+import { getPersonalDetails } from "./PersonalForm.mjs";
+import { getGovtForm } from "./GovtForm.mjs";
 
 window.onload = () => {
     addHeader();
+    getPersonalDetails();
+    getGovtForm();
 }
