ff5dd889a4e9ae6aec836e84e20a1320850a80f5
manalidalvi12 <manalidalvi12@gmail.com>
2022-09-06 17:03:10 +0530

    UPDATE: LoginForm created

---
+++ js/components/LoginForm.mjs
@@ -0,0 +1,29 @@
+export function getLoginInfo(){
+
+    const br = document.createElement("br");
+
+    const form = document.createElement("form");
+    form.setAttribute("method","post");
+
+    const uname = document.createElement("input");
+    uname.setAttribute("type","text");
+    uname.setAttribute("name","uname");
+    uname.setAttribute("placeholder","Username");
+
+    const pass = document.createElement("input");
+    pass.setAttribute("type","password");
+    pass.setAttribute("name","pass");
+    pass.setAttribute("placeholder","Password");
+
+    const submit = document.createElement("input");
+    submit.setAttribute("type","submit");
+    submit.setAttribute("value","Next");
+
+    form.appendChild(uname);
+    form.appendChild(br.cloneNode());
+    form.appendChild(pass);
+    form.appendChild(br.cloneNode());
+    form.appendChild(submit);
+
+    document.getElementById("root").appendChild(form);
+}
