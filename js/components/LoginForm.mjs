b13c25388eade877df28656c75b7ea0724e2edf4
varun-316 <varun.pandey@think360.ai>
2022-09-07 10:32:17 +0530

    UPDATE: Implemented login session and local storage

--- js/components/LoginForm.mjs
+++ js/components/LoginForm.mjs
@@ -1,24 +1,68 @@
 export function getLoginInfo(){
     const form = document.createElement("form");
-    form.setAttribute("method","post");
+    // form.setAttribute("method", "post");
+
 
     const uname = document.createElement("input");
     uname.setAttribute("type","text");
     uname.setAttribute("name","uname");
+    uname.setAttribute("id", "uname");
+    uname.setAttribute("required", "");
     uname.setAttribute("placeholder","Username");
 
     const pass = document.createElement("input");
     pass.setAttribute("type","password");
     pass.setAttribute("name","pass");
+    pass.setAttribute("required", "");
+    pass.setAttribute("id", "pass");
     pass.setAttribute("placeholder","Password");
 
     const submit = document.createElement("input");
     submit.setAttribute("type","submit");
-    submit.setAttribute("value","Next");
+    submit.setAttribute("value","Login");
 
     form.appendChild(uname);
     form.appendChild(pass);
     form.appendChild(submit);
 
+    
+
     document.getElementById("root").appendChild(form);
+
+    form.onsubmit = async (e) => {
+        await setLoginValues();        
+        // window.location.reload();
+    }
+}
+
+const setLoginValues = async () => {
+    const uname = document.getElementById("uname").value;
+    const pass = document.getElementById("pass").value;
+    await setSession(uname, pass);    
+    await setLocal(uname, pass);
+}
+
+const setSession = async (uname, pass) => {
+    window.sessionStorage.setItem("username", uname);
+    window.sessionStorage.setItem("password", pass);
+}
+
+const setLocal = async (uname, pass) => {
+    const user = window.localStorage.getItem(uname);
+
+    if(!user) {
+        const userObj = {
+            "uname": uname,
+            "pass": pass,
+            "fname": "",
+            "lname": "",
+            "contact": "",
+            "email": "",
+            "address": "",
+            "aadhaar": "",
+            "pan": "",
+            "stage": 1
+        }
+        window.localStorage.setItem(uname, JSON.stringify(userObj));
+    }
 }
